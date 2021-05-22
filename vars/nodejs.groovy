90f7585482888d21933e970499a82f2e8ad0c53e
Naresh REDDY <nareshreddyboreddy904@gmail.com>
2021-05-22 17:17:16 +1000

    hi

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -17,8 +17,6 @@ def call(String component) {
 }
           stage('Sonar Scan') {
               steps {
-                  script {
-                     sonar.scan(component)
+                  sh "sonar-scanner -Dsonar.projectKey=${component} -Dsonar.sources=.-Dsonar.host.url=http://172.31.73.89:9000 -Dsonar.login=aebe06f7e87214c54e1edbe1391548a131e3d467"
         }
        }
-     }
