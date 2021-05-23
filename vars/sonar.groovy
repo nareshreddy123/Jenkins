8d9f058fa483dfc47ac77fc02d61c606b5761508
Naresh REDDY <nareshreddyboreddy904@gmail.com>
2021-05-23 13:26:04 +1000

    test full url - revert

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,4 +1,4 @@
 def scan(component) {
-    sh "sonar-scanner -Dsonar.projectKey=${component} -Dsonar.sources=.-Dsonar.host.url=http://3.236.234.79:9000/projects" +
+    sh "sonar-scanner -Dsonar.projectKey=${component} -Dsonar.sources=.-Dsonar.host.url=http://3.236.234.79:9000" +
             " -Dsonar.login=aebe06f7e87214c54e1edbe1391548a131e3d467"
 }
