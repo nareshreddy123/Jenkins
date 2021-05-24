a6a4da2afa2e3b444a75b65bb770c20414e1877a
Naresh REDDY <nareshreddyboreddy904@gmail.com>
2021-05-25 07:55:02 +1000

    hi

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,4 +1,4 @@
 def scan(component) {
-    sh "sonar-scanner -Dsonar.projectKey=${component} -Dsonar.sources=. -Dsonar.host.url=http://3.239.7.29:9000 -Dsonar.login=a694a65206d41683c5d3f0ac83a6e518fadebd4a"
+    sh "sonar-scanner -Dsonar.projectKey=${component} -Dsonar.sources=. -Dsonar.host.url=http://3.239.7.29:9000 -Dsonar.login=5683fc7da7c72f6d7588dcf27abd91a700c3385c"
 
 }
