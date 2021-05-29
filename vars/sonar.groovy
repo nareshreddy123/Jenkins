629f75c81fd3e30f3abe7d868c5d2cb47c6c8999
Naresh REDDY <nareshreddyboreddy904@gmail.com>
2021-05-29 19:47:25 +1000

    New Token

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,4 +1,8 @@
 def scan(component) {
 //    sh 'sonar-scanner -Dsonar' '.projectKey=${component}' 'Dsonar.sources=.' '-Dsonar.host.url=http://3.236.21.181:9000 -Dsonar.login=298e4100451f7c46ec8393a40388271883b3e455'
-sh " sonar-scanner -Dsonar.projectKey=${component} -Dsonar.sources=. -Dsonar.host.url=http://3.236.21.181:9000 -Dsonar.login=4c74445d6f0232091da066dd3be902b418fdd436"
+sh "sonar-scanner " +
+        "  -Dsonar.projectKey=cart " +
+        "  -Dsonar.sources=. " +
+        "  -Dsonar.host.url=http://3.236.21.181:9000 " +
+        "  -Dsonar.login=8ad6674d77f1f9e3c4be53daa4705d321a9d0b0c"
 }
