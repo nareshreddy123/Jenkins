9bf275ed563a4b77b4fa35774a8358c8fd0de789
Naresh REDDY <nareshreddyboreddy904@gmail.com>
2021-05-30 16:12:48 +1000

    heyy

--- vars/java.groovy
+++ vars/java.groovy
@@ -10,37 +10,20 @@ def call(String component) {
 
             stage('Compile Code') {
                 steps {
-                    withMaven(maven: 'maven') {
 
-                        stage('Checkout') {
-                            git url: 'https://github.com/nareshreddy123/devopsnaresh.git', credentialsId: 'nareshreddy123', branch: 'main'
-                            sh 'mvn compile'
-                        }
-
-                    }
-
-
-                    stage('Build Code') {
-                        steps {
-
-                            stage('Build') {
-
-                                dir('project-dir') {
-                                    sh 'mvn clean install'
-
-                                    def pom = readMavenPom file: 'pom.xml'
-
-                                    print pom.version
-                                    env.version = pom.version
-                                    sh 'mvn package'
-                                }
-                            }
-
-                        }
-
-                    }
+                    sh 'mvn compile'
                 }
+
             }
-        }
+
+
+        stage('Build Code') {
+               steps {
+                 sh 'mvn package'
     }
+}
+
+}
+
+}
 }
