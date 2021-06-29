d96023648666b4e559b855377ac291170a20912b
Naresh REDDY <nareshreddyboreddy904@gmail.com>
2021-06-29 18:42:46 +1000

    sonar

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,6 +1,12 @@
 def scan(component) {
-    sh "sonar-scanner -Dsonar.projectKey=${component} -Dsonar.sources=. -Dsonar.host.url=http://3.214.215.107:9000 -Dsonar.login=9eaadaf0b840df743f2715fe9a7aee549b19ba95"
+
+    if (component == "shipping") {
+        sh "sonar-scanner -Dsonar.projectKey=${component} -Dsonar.sources=. -Dsonar.java.binaries=./target -Dsonar.host.url=http://3.214.215.107:9000 -Dsonar.login=9eaadaf0b840df743f2715fe9a7aee549b19ba95"
+    } else {
+        sh "sonar-scanner -Dsonar.projectKey=${component} -Dsonar.sources=. -Dsonar.host.url=http://3.214.215.107:9000 -Dsonar.login=9eaadaf0b840df743f2715fe9a7aee549b19ba95"
+    }
 }
-def report(component) {
-    sh "sonar-quality-gate.sh admin admin123 3.214.215.107 ${component}"
-}
+
+    def report(component) {
+        sh "sonar-quality-gate.sh admin admin123 172.31.66.121 ${component}"
+    }
