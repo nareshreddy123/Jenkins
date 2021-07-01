f9c5b2f756aac2cae97847e255a4ee5550f786f9
Naresh REDDY <nareshreddyboreddy904@gmail.com>
2021-07-01 20:53:01 +1000

    hi

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -14,7 +14,6 @@ def call(String component) {
             stage('Prepare Artifacts') {
 
                 steps {
-
                     sh """
                       zip -r ${component}-${TAG}.zip node_modules server.js
                    """
