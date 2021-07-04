a5f6e555192e2561ba1080a4f8a8e6e5886f4411
Naresh REDDY <nareshreddyboreddy904@gmail.com>
2021-07-04 11:08:36 +1000

    hi

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,53 +1,64 @@
 def call(String component) {
     pipeline {
         agent any
+
         triggers {
-                    pollSCM('H/2 * * * *')
+            pollSCM('H/2 * * * *')
+        }
+
+        parameters {
+            string(name: 'TAG', defaultValue: 'NA', description: 'Tag to Release')
+        }
+
+        stages {
+
+            stage('Download Dependencies') {
+                steps {
+                    sh 'npm install'
                 }
+            }
 
-
-                stages {
-            stage('download dependencies')
-                    {
-                        steps
-                                {
-                                    sh 'npm install'
-                                }
-                    }
+//      stage('Sonar Scan') {
+//        steps {
+//          script {
+//            sonar.scan(component)
+//          }
+//        }
+//      }
+//
+//      stage('Sonar Quality Gate Status') {
+//        steps {
+//          catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
+//            script {
+//              sonar.report(component)
+//            }
+//          }
+//        }
+//      }
 
             stage('Prepare Artifacts') {
-
+                when {
+                    expression {
+                        env.TAG != "NA"
+                    }
+                }
                 steps {
                     sh """
-            zip -r ${component}.zip node_modules server.js
+            zip -r ${component}-${TAG}.zip node_modules server.js
           """
                 }
             }
 
             stage('Upload Nexus Artifacts') {
-
-                steps {
-                    sh "curl -f -v -u admin:admin --upload-file ${component}.zip http://3.85.224.77:8081/repository/${component}/${component}.zip"
-                }
-            }
-
-
-            stage('Sonar Scan') {
-                steps {
-                    script {
-                        sonar.scan(component)
+                when {
+                    expression {
+                        env.TAG != "NA"
                     }
+                }
+                steps {
+                    sh "curl -f -v -u admin:admin --upload-file ${component}-${TAG}.zip http://3.85.224.77:8081/repository/${component}/${component}-${TAG}.zip"
                 }
             }
         }
     }
 }
-             stage('Sonar Quality Gate Status') {
-                 steps {script {
-                     sonar.report(component)
-
-                 }
-
-                 }
-
-             }
